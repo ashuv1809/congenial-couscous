99b4a20eccc33b63054ec9e715619de0289b4e7b
ashuv1809 <107414131+ashuv1809@users.noreply.github.com>
2022-07-11 01:25:09 +0530

    Create Triangular star pattern.cpp

---
+++ Triangular star pattern.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+    int N;
+    cin>>N;
+    int i=1;
+    while(i<=N)
+    {
+       int j=1;
+        while(j<=i)
+        {
+            cout<<"*";
+            j++;
+        }
+        cout<<endl;
+        i++;
+    }
+
+       /*  Read input as specified in the question.
+	* Print output as specified in the question.
+	*/
+  
+}
+
+
