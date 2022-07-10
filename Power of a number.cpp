bc59b57dd2529801d26c115d484a42a644bb332e
ashuv1809 <107414131+ashuv1809@users.noreply.github.com>
2022-07-11 01:18:18 +0530

    Create Power of a number.cpp

---
+++ Power of a number.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int n,x,a=1;
+    cin>>x>>n;
+    int i=1;
+    while(i<=n)
+    {
+        a=a*x;
+        i++;
+    
+    }
+    cout<<a;
+	// Write your code here
+	
+}
