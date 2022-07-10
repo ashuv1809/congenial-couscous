e1a84b45df110f43f773400dc48eae129d7e8869
ashuv1809 <107414131+ashuv1809@users.noreply.github.com>
2022-07-11 01:28:51 +0530

    Create Alphabetical pattern.cpp

---
+++ Alphabetical pattern.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+    int N;
+    cin>>N;
+    int i=1;
+    int j;
+    while(i<=N)
+    {
+        j=1;
+        char ch='A'+i-1;
+        while(j<=i)
+        {
+            cout<<ch;
+            j++;
+        }
+        cout<<endl;
+        i++;
+    }
+    
+    /*  Read input as specified in the question.
+     * Print output as specified in the question.
+     */
+    
+}
+
