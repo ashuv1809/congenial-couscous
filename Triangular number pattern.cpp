4a4d723a4a4068a5a99faee5a4e22d29013d25bf
ashuv1809 <107414131+ashuv1809@users.noreply.github.com>
2022-07-11 01:26:49 +0530

    Create Triangular number pattern.cpp

---
+++ Triangular number pattern.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+    int N,i,j;
+    cin>>N;
+    i=1;
+    while(i<=N)
+    {
+        j=1;
+        while(j<=i)
+        {
+            cout<<i;
+            j++;
+        }
+        cout<<endl;
+        i++;
+    }
+
+       /*  Read input as specified in the question.
+	* Print output as specified in the question.
+	*/
+  
+}
+
+
